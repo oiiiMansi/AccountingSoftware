29a5e9660c068584de202ecbe55c890b204e0545
oiiiMansi <sainimansi360@gmail.com>
2025-05-10 08:55:34 +0530

    database updated

--- database.sql
+++ database.sql
@@ -26,16 +26,17 @@ CREATE TABLE `billed_purchases` (
   `id` int NOT NULL AUTO_INCREMENT,
   `vendor_name` varchar(255) NOT NULL,
   `amount` decimal(10,2) NOT NULL,
+  `paid_amount` decimal(10,2) DEFAULT '0.00',
   `quantity` int DEFAULT '1',
   `payment_type` enum('Cash','Credit') DEFAULT 'Cash',
-  `payment_status` enum('Pending','Paid') DEFAULT 'Paid',
+  `payment_status` enum('Pending','Partially Paid','Paid') DEFAULT 'Paid',
   `gst_type` enum('CGST_SGST','IGST') DEFAULT 'CGST_SGST',
   `gst_percentage` decimal(5,2) DEFAULT '18.00',
   `date` date NOT NULL,
   `description` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +45,7 @@ CREATE TABLE `billed_purchases` (
 
 LOCK TABLES `billed_purchases` WRITE;
 /*!40000 ALTER TABLE `billed_purchases` DISABLE KEYS */;
-INSERT INTO `billed_purchases` VALUES (6,'Priya Nehra',10000.00,2,'Cash','Paid','CGST_SGST',18.00,'2025-04-16','','2025-04-16 07:17:17'),(7,'Priya Nehra',100.00,1,'Cash','Paid','CGST_SGST',18.00,'2025-04-20','','2025-04-20 02:52:36'),(8,'roxuu',1000000.00,1,'Cash','Paid','CGST_SGST',18.00,'2025-04-23','','2025-04-23 08:19:00');
+INSERT INTO `billed_purchases` VALUES (6,'Priya Nehra',10000.00,10000.00,2,'Cash','Paid','CGST_SGST',18.00,'2025-04-16','','2025-04-16 07:17:17'),(7,'Priya Nehra',100.00,100.00,1,'Cash','Paid','CGST_SGST',18.00,'2025-04-20','','2025-04-20 02:52:36'),(8,'roxuu',1000000.00,1000000.00,1,'Cash','Paid','CGST_SGST',18.00,'2025-04-23','','2025-04-23 08:19:00'),(9,'cre',10000.00,10000.00,1,'Cash','Paid','CGST_SGST',18.00,'2025-05-09','','2025-05-09 09:28:45');
 /*!40000 ALTER TABLE `billed_purchases` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,14 +67,15 @@ CREATE TABLE `bills` (
   `basic_amount` decimal(10,2) DEFAULT NULL,
   `quantity` int DEFAULT '1',
   `payment_type` enum('Cash','Credit') DEFAULT 'Cash',
-  `payment_status` enum('Pending','Paid') DEFAULT 'Paid',
+  `payment_status` enum('Pending','Partially Paid','Paid') DEFAULT 'Paid',
   `gst_type` varchar(20) DEFAULT NULL,
   `gst_percentage` decimal(5,2) DEFAULT NULL,
   `gst_amount` decimal(10,2) DEFAULT NULL,
   `total_amount` decimal(10,2) DEFAULT NULL,
+  `paid_amount` decimal(10,2) DEFAULT '0.00',
   `payment_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,7 +84,7 @@ CREATE TABLE `bills` (
 
 LOCK TABLES `bills` WRITE;
 /*!40000 ALTER TABLE `bills` DISABLE KEYS */;
-INSERT INTO `bills` VALUES (10,'roxuu',NULL,'2025-04-15 18:30:00','7488379237','rgerggeg','gegerg',100.00,1,'Cash','Paid','CGST_SGST',18.00,18.00,118.00,NULL),(11,'roxuu',NULL,'2025-04-16 18:30:00','roxuu','400','kk',400.00,1,'Cash','Paid','CGST_SGST',18.00,72.00,472.00,NULL);
+INSERT INTO `bills` VALUES (10,'roxuu',NULL,'2025-04-15 18:30:00','7488379237','rgerggeg','gegerg',100.00,1,'Cash','Paid','CGST_SGST',18.00,18.00,118.00,118.00,NULL),(11,'roxuu',NULL,'2025-04-16 18:30:00','roxuu','400','kk',400.00,1,'Cash','Paid','CGST_SGST',18.00,72.00,472.00,472.00,NULL),(28,'a',NULL,'2025-05-08 18:30:00','7488379237','aaaa','aaaa',1000000.00,1,'Credit','Partially Paid','CGST_SGST',18.00,180000.00,1180000.00,70000.00,NULL),(29,'Shyam',NULL,'2025-05-09 09:39:46','7488379237','sdad','ads',120000.00,1,'Cash','Paid','CGST_SGST',18.00,21600.00,141600.00,0.00,NULL);
 /*!40000 ALTER TABLE `bills` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -191,7 +193,7 @@ CREATE TABLE `employees` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -227,7 +229,7 @@ CREATE TABLE `expenses` (
 
 LOCK TABLES `expenses` WRITE;
 /*!40000 ALTER TABLE `expenses` DISABLE KEYS */;
-INSERT INTO `expenses` VALUES (2,'samosa',350.00,'Other','2025-05-01 18:30:00',63.00);
+INSERT INTO `expenses` VALUES (2,'samosa',355.00,'Other','2025-05-01 18:30:00',63.90);
 /*!40000 ALTER TABLE `expenses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -275,8 +277,9 @@ CREATE TABLE `non_billed_sales` (
   `item_details` text,
   `quantity` int DEFAULT '1',
   `amount` decimal(10,2) NOT NULL,
+  `paid_amount` decimal(10,2) DEFAULT '0.00',
   `payment_type` enum('Cash','Credit') DEFAULT 'Cash',
-  `payment_status` enum('Pending','Paid') DEFAULT 'Paid',
+  `payment_status` enum('Pending','Partially Paid','Paid') DEFAULT 'Paid',
   `date` date NOT NULL,
   `notes` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -291,7 +294,7 @@ CREATE TABLE `non_billed_sales` (
 
 LOCK TABLES `non_billed_sales` WRITE;
 /*!40000 ALTER TABLE `non_billed_sales` DISABLE KEYS */;
-INSERT INTO `non_billed_sales` VALUES (3,'Shyam','7896541236','cello bottle',1,601.00,'Cash','Paid','2025-04-15','','2025-04-16 07:36:33',NULL),(4,'asd','5484874546','nothing',1,10233.00,'Cash','Paid','2025-04-19','','2025-04-19 08:45:12',NULL);
+INSERT INTO `non_billed_sales` VALUES (3,'Shyam','7896541236','cello bottle',1,601.00,601.00,'Cash','Paid','2025-04-15','','2025-04-16 07:36:33',NULL),(4,'asd','5484874546','nothing',1,10233.00,10233.00,'Cash','Paid','2025-04-19','','2025-04-19 08:45:12',NULL);
 /*!40000 ALTER TABLE `non_billed_sales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -306,9 +309,10 @@ CREATE TABLE `purchases` (
   `id` int NOT NULL AUTO_INCREMENT,
   `vendor_name` varchar(255) NOT NULL,
   `amount` decimal(10,2) NOT NULL,
+  `paid_amount` decimal(10,2) DEFAULT '0.00',
   `quantity` int DEFAULT '1',
   `payment_type` enum('Cash','Credit') DEFAULT 'Cash',
-  `payment_status` enum('Pending','Paid') DEFAULT 'Paid',
+  `payment_status` enum('Pending','Partially Paid','Paid') DEFAULT 'Paid',
   `date` date NOT NULL,
   `item_details` text NOT NULL,
   `notes` text,
@@ -323,7 +327,7 @@ CREATE TABLE `purchases` (
 
 LOCK TABLES `purchases` WRITE;
 /*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
-INSERT INTO `purchases` VALUES (3,'Raam',300.00,1,'Cash','Paid','2025-04-01','Cello Bottle','','2025-04-16 07:31:34'),(4,'roxuu',350.00,7,'Cash','Paid','2025-04-17','ii','ii','2025-04-17 09:00:47'),(5,'roxuu',10000.00,1,'Cash','Paid','2025-04-24','xasx','axsax','2025-04-23 08:14:25');
+INSERT INTO `purchases` VALUES (3,'Raam',300.00,300.00,1,'Cash','Paid','2025-04-01','Cello Bottle','','2025-04-16 07:31:34'),(4,'roxuu',350.00,350.00,7,'Cash','Paid','2025-04-17','ii','ii','2025-04-17 09:00:47'),(5,'roxuu',10000.00,10000.00,1,'Cash','Paid','2025-04-24','xasx','axsax','2025-04-23 08:14:25');
 /*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -401,7 +405,7 @@ CREATE TABLE `stock` (
 
 LOCK TABLES `stock` WRITE;
 /*!40000 ALTER TABLE `stock` DISABLE KEYS */;
-INSERT INTO `stock` VALUES (4,'cello bottle',2,'2025-04-16 07:30:39');
+INSERT INTO `stock` VALUES (4,'cello bottle',1,'2025-04-16 07:30:39');
 /*!40000 ALTER TABLE `stock` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -414,7 +418,7 @@ DROP TABLE IF EXISTS `transactions`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `transactions` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL,
+  `date` datetime NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `description` varchar(255) NOT NULL,
   `transaction_type` enum('credit','debit') NOT NULL DEFAULT 'credit',
@@ -422,8 +426,9 @@ CREATE TABLE `transactions` (
   `reference_type` varchar(50) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `payment_note` text,
+  `narration` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -432,7 +437,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-INSERT INTO `transactions` VALUES (12,'2025-04-16',300.00,'Purchase from Ram: Cello Bottle','debit',3,'purchase','2025-04-16 07:31:34',NULL),(13,'2025-04-15',600.00,'Sale to Shyam: cello bottle','credit',3,'non_billed_sale','2025-04-16 07:36:33',NULL),(14,'2025-04-17',472.00,'Bill to roxuu','credit',11,'bill','2025-04-17 08:40:57',NULL),(17,'2025-04-19',10233.00,'Sale to asd: nothing (Qty: 1)','credit',4,'non_billed_sale','2025-04-19 08:45:12',NULL),(18,'2025-04-20',118.00,'Purchase from Priya Nehra:  (Qty: 1)','debit',7,'billed_purchase','2025-04-20 02:52:36',NULL),(25,'2025-04-24',10000.00,'Purchase from roxuu: xasx (Qty: 1)','debit',5,'purchase','2025-04-23 08:14:25',NULL),(26,'2025-04-23',1180000.00,'Purchase from roxuu:  (Qty: 1)','debit',8,'billed_purchase','2025-04-23 08:19:00',NULL);
+INSERT INTO `transactions` VALUES (12,'2025-04-16 00:00:00',300.00,'Purchase from Ram: Cello Bottle','debit',3,'purchase','2025-04-16 07:31:34',NULL,NULL),(13,'2025-04-15 00:00:00',600.00,'Sale to Shyam: cello bottle','credit',3,'non_billed_sale','2025-04-16 07:36:33',NULL,NULL),(14,'2025-04-17 00:00:00',472.00,'Bill to roxuu','credit',11,'bill','2025-04-17 08:40:57',NULL,NULL),(17,'2025-04-19 00:00:00',10233.00,'Sale to asd: nothing (Qty: 1)','credit',4,'non_billed_sale','2025-04-19 08:45:12',NULL,NULL),(18,'2025-04-20 00:00:00',118.00,'Purchase from Priya Nehra:  (Qty: 1)','debit',7,'billed_purchase','2025-04-20 02:52:36',NULL,NULL),(29,'2025-05-09 00:00:00',70000.00,'Partial payment for bill #28','credit',28,'bill_payment','2025-05-09 09:21:51',NULL,NULL),(30,'2025-05-09 00:00:00',11800.00,'Purchase from cre:  (Qty: 1)','debit',9,'billed_purchase','2025-05-09 09:28:45',NULL,NULL),(31,'2025-05-09 00:00:00',5000.00,'Partial payment for billed purchase #9','debit',9,'billed_purchase_payment','2025-05-09 09:29:06',NULL,NULL),(32,'2025-05-09 00:00:00',5000.00,'Partial payment for billed purchase #9','debit',9,'billed_purchase_payment','2025-05-09 09:29:16',NULL,NULL),(33,'2025-05-09 15:09:46',141600.00,'Bill for Shyam','credit',29,'bill','2025-05-09 09:39:46',NULL,NULL);
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -474,4 +479,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-02  9:14:57
+-- Dump completed on 2025-05-10  8:52:07
