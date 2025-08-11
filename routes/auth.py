1f4124bc7b95955966bb325cde6d1ac0ba4b00b5
oiiiMansi <sainimansi360@gmail.com>
2025-08-11 17:36:05 +0530

    minor change

--- routes/auth.py
+++ routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User  # Import User class from models
+from models import User 
 
 auth = Blueprint("auth", __name__)
 
